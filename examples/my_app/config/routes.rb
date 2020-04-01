aa68926582fbb82689fad2c298a35245cb3c224e
Peter Saxton <peterhsaxton@gmail.com>
2020-04-01 13:37:41 +0100

    add welcome controller

--- examples/my_app/config/routes.rb
+++ examples/my_app/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
 end
